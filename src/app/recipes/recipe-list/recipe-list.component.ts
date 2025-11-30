bffac9b2f87408a703aaef1992c115dd33f4d463
Gela Pipia <gelaphiphia10@gmail.com>
2025-12-01 00:34:19 +0400

    feat: recipe list and details added

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent {
-  recipres = []
+  recipes: Recipe[] = [
+    new Recipe("lebio", "batumela lobio batumidan", "https://i.pinimg.com/236x/ce/31/48/ce3148a050a13225897047eefe9b0bef.jpg"),
+    new Recipe("lebio", "batumela lobio batumidan", "https://i.pinimg.com/236x/ce/31/48/ce3148a050a13225897047eefe9b0bef.jpg"),
+    new Recipe("lebio", "batumela lobio batumidan", "https://i.pinimg.com/236x/ce/31/48/ce3148a050a13225897047eefe9b0bef.jpg"),
+  ]
+
+
 }
