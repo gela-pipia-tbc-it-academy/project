1ab37ee19f2b261a0dc90ee35e8e1c9c7030a980
Gela Pipia <gelaphiphia10@gmail.com>
2025-12-01 00:34:50 +0400

    feat: add shopping-list content

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Ingredient } from "../shared/ingredient.model";
 
 @Component({
     selector: "app-shopping-list",
@@ -6,4 +7,12 @@ import { Component } from "@angular/core";
     styleUrls: ["./shopping-list.component.css"]
 })
 
-export class ShoppingListComponent {}
+export class ShoppingListComponent implements OnInit {
+    ingredients: Ingredient[] = [
+        new Ingredient("Onion", 6),
+        new Ingredient("Greens", 3)
+    ];
+
+    ngOnInit(): void {
+    }
+}
